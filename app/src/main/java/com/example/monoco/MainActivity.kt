00c2a849821497d97f05c73dfa8f7986ae6f556e
Willian Anderson da Rocha <rocha.willian@pucpr.edu.br>
2026-08-27 22:20:49 -0300

    feat(welcome): add initial welcome screen

--- app/src/main/java/com/example/monoco/MainActivity.kt
+++ app/src/main/java/com/example/monoco/MainActivity.kt
@@ -4,24 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.savedstate.SavedState
 import com.example.monoco.ui.theme.MonocoTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            MonocoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+
+        setContent{
+            MonocoTheme{
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    innerPadding -> WelcomeScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +42,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun WelcomeScreen(modifier: Modifier = Modifier) {
+    Column (
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(horizontal = 32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Monoco",
+            fontSize = 48.sp,
+            style = MaterialTheme.typography.headlineLarge
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Aventuras narrativas conduzidas por Inteligência Artificial.",
+            style = MaterialTheme.typography.bodyLarge,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = modifier.height(32.dp))
+
+        Button(
+            onClick = {}
+        ) {
+            Text(text = "Começar aventura")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun WelcomeScreenPreview(){
     MonocoTheme {
-        Greeting("Android")
+        WelcomeScreen()
     }
 }
